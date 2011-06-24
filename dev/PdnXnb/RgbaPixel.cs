fde852c1a32215775e886dc66770e34b1d8c35a2
r2d2rigo_cp <SND\r2d2rigo_cp>
2011-06-24 20:24:06 +0000

    Initial check-in.

---
+++ dev/PdnXnb/RgbaPixel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PdnXnb
+{
+    public class RgbaPixel : BasePixel
+    {
+        private byte[] rgbaData;
+
+        public override int DataSize
+        {
+            get { return 4; }
+        }
+
+        public RgbaPixel(byte[] pixelData)
+        {
+            this.Data = pixelData;
+        }
+
+        public override BasePixel ToRgba()
+        {
+            return this;
+        }
+    }
+}
